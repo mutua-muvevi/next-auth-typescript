3b7e9048fa593e59cb2a03375b8791eb794af252
mutua-muvevi <samstore637@gmail.com>
2024-08-08 04:53:14 +0300

    build the admin layout

---
+++ src/app/admin/loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { LoadingScreen } from "@/components/loading_screen";
+
+// ----------------------------------------------------------------------
+
+const Loading = () => {
+	return <LoadingScreen />;
+};
+
+export default Loading;
